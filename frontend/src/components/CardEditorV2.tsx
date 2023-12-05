4d193e1362ca076826c53cb07276eec507bfda7b
Alex T <79433869+hereAlexT@users.noreply.github.com>
2023-12-05 12:36:05 +1100

    cardeidtorv2 can handle pic input

--- frontend/src/components/CardEditorV2.tsx
+++ frontend/src/components/CardEditorV2.tsx
@@ -7,15 +7,22 @@ import {
     IonRow,
     IonGrid,
     IonTextarea,
-    IonIcon
+    IonIcon,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonImg,
+    IonThumbnail
 
 } from '@ionic/react';
 import { Note, UnSyncedNote } from '../shared/types';
-import { 
-    arrowForwardOutline as  arrowForwardOutlineIcon,
-    imageOutline as imageOutlineIcon
- } from 'ionicons/icons';
+import {
+    arrowForwardOutline as arrowForwardOutlineIcon,
+    imageOutline as imageOutlineIcon,
+    imageSharp
+} from 'ionicons/icons';
 import { v4 as uuidv4 } from 'uuid';
+import {Camera, CameraResultType, Photo} from '@capacitor/camera';
 
 interface CardEditorV2Props {
     onSubmit: (noteContent: Note) => void;
@@ -25,7 +32,7 @@ interface CardEditorV2Props {
 
 
 
-const CardEditorV2: React.FC<CardEditorV2Props> = ({onSubmit, note, isOnline=true}) => {
+const CardEditorV2: React.FC<CardEditorV2Props> = ({ onSubmit, note, isOnline = true }) => {
     const [content, setContent] = useState('');
 
     const HandleOnSubmit = () => {
@@ -38,6 +45,22 @@ const CardEditorV2: React.FC<CardEditorV2Props> = ({onSubmit, note, isOnline=tru
         onSubmit(newNote);
         setContent('');
     }
+
+    const handleImageButtonClick = async () => {
+        const image = await Camera.getPhoto({
+            quality: 90,
+            allowEditing: false,
+            resultType: CameraResultType.Uri
+        });
+
+        if (image.webPath) {
+            setImages(prevImages => [...prevImages, image.webPath!]);
+        }
+    };
+
+    const [images, setImages] = useState<string[]>([
+
+    ]);
 
     return (
         <IonGrid class="ion-no-padding">
@@ -56,12 +79,22 @@ const CardEditorV2: React.FC<CardEditorV2Props> = ({onSubmit, note, isOnline=tru
                     />
                 </IonCol>
             </IonRow>
+            <IonRow className='ion-justify-content-start'>
+                {images.map((image, index) => (
+                    <IonCol key={index}>
+                        <IonImg alt={`Image ${index}`} src={image} />
+                    </IonCol>
+                ))}
+            </IonRow>
+
+
             <IonRow>
-            <IonCol size="1" className='' >
+                <IonCol size="1" className='' >
                     <IonButton
                         color="primary"
                         size="small"
                         fill="clear"
+                        onClick={handleImageButtonClick}
                         className='circular-button'>
                         <IonIcon color="dark" className="m-0 p-0" size="small" slot="icon-only" icon={imageOutlineIcon} />
                     </IonButton>
