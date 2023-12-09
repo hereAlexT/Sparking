1cc5c6c9823275ed19bdfe9e920b919ead4cc090
Alex T <79433869+hereAlexT@users.noreply.github.com>
2023-12-10 10:19:58 +1100

    fix typo

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -2,4 +2,4 @@
 export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 export const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 export const HCAPTCHA_SITE_KEY = import.meta.env.VITE_HCAPTCHA_SITE_KEY;
-export const DOMIAN_NAME = import.meta.env.VITE_DOMAIN_NAME;
+export const DOMAIN_NAME = import.meta.env.VITE_DOMAIN_NAME;
