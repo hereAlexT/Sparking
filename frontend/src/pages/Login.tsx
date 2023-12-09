daf6756319288322a574084dc5377029ed90226e
Alex T <79433869+hereAlexT@users.noreply.github.com>
2023-12-09 20:04:44 +1100

    login finished

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -22,14 +22,15 @@ import { HCAPTCHA_SITE_KEY } from '../config';
 
 
 const Login: React.FC = () => {
+
     const history = useHistory();
-    const [email, setEmail] = useState("demo@linklabs.app");
-    const [password, setPassword] = useState("Password1@");
-    const [isLoginFailed, setLoginFailed] = useState(false);
     const { login, isAuthenticated, session, user } = useAuth();
     const { isOnline } = useMeta();
 
 
+    const [email, setEmail] = useState("demo@linklabs.app");
+    const [password, setPassword] = useState("Password1@");
+    const [isLoginFailed, setLoginFailed] = useState(false);
 
 
     /** Captcha */
@@ -100,7 +101,48 @@ const Login: React.FC = () => {
                         </IonButtons> */}
                     </IonToolbar>
                 </IonHeader>
-                <IonGrid className='ion-padding'>
+                <div className='grid grid-cols-1 p-4'>
+                    <div className='mb-4'>
+                        <h1 className='text-3xl font-semibold'>Welcome to Sparking.</h1>
+                    </div>
+
+                    <form onSubmit={handleSubmit}>
+                        <div className="mb-5">
+                            <label
+                                htmlFor="email"
+                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                            <input
+                                type="email"
+                                id="email"
+                                onChange={(e) => setEmail(e.target.value)}
+                                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="Your email here.." required />
+                        </div>
+                        <div className="mb-5">
+                            <label
+                                htmlFor="password"
+                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+                            <input
+                                type="password"
+                                id="password"
+                                onChange={(e) => setPassword(e.target.value)}
+                                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
+                        </div>
+                        <HCaptcha
+                            ref={captcha}
+                            sitekey={HCAPTCHA_SITE_KEY}
+                            onVerify={(token) => { setCaptchaToken(token) }} />
+                        <button type="submit" className="mt-4 w-full text-white bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
+                    </form>
+                    <div className='mt-4'>
+                        <IonRouterLink>Forgot password?</IonRouterLink>
+                    </div>
+                    <div className='mt-2'>
+                        <IonRouterLink routerLink='/signup' >Don't have an account? Signup</IonRouterLink>
+                    </div>
+                </div>
+
+
+                {/* <IonGrid className='ion-padding'>
                     <form onSubmit={handleSubmit}>
                         <IonRow>
                             <IonCol>
@@ -199,7 +241,7 @@ const Login: React.FC = () => {
                     message="We don't recognize you, try again or create a new account?"
                     buttons={['Action']}
                     onDidDismiss={() => setLoginFailed(false)}
-                ></IonAlert>
+                ></IonAlert> */}
             </IonContent>
         </IonPage>
     );
