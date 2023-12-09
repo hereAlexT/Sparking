ee8abbf8a7f77ea42a247daebf52dc43b3007a8b
Alex T <79433869+hereAlexT@users.noreply.github.com>
2023-12-10 10:24:12 +1100

    forgot password

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -60,11 +60,16 @@ const Login: React.FC = () => {
 
     /** Handle Forget password */
     const handleForgetPassword = async () => {
+        if (!email) {
+            alert("Please enter your email")
+            return;
+        }
+
         try {
             const { data } = await resetPasswordForEmail(email);
             console.debug(data)
         } catch (error) {
-            alert ("Failed to reset password: \n" + error)
+            alert("Failed to reset password: \n" + error)
             console.error(error);
         }
     }
@@ -115,9 +120,12 @@ const Login: React.FC = () => {
                     <button
                         onClick={() => loginWithGoogle()}
                         className="mt-4 w-full text-white bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Continue with Google</button>
-                    <div className='mt-4'>
-                        <IonRouterLink>Forgot password?</IonRouterLink>
-                    </div>
+               
+                    <span
+                        onClick={handleForgetPassword}
+                        className='mt-4 w-full text-blue-500 hover:underline cursor-pointer '>
+                        Forgot password?
+                    </span>
                     <div className='mt-2'>
                         <IonRouterLink routerLink='/signup' >Don't have an account? Signup</IonRouterLink>
                     </div>
