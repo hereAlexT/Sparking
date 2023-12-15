575a25c1d1ebd6e7ad983859cff980f64ac73ce8
Alex T <79433869+hereAlexT@users.noreply.github.com>
2023-12-15 14:04:36 +1100

    set up prettier with plugins

---
+++ frontend/prettier.config.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+    plugins: [
+        "@trivago/prettier-plugin-sort-imports",
+        "prettier-plugin-svelte",
+        'prettier-plugin-tailwindcss'
+    ],
+}
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,71 +1,69 @@
+import "./App.css";
+import Logout from "./components/Logout";
+import Menu from "./components/Menu";
+import { GA_MEASUREMENT_ID } from "./config";
+import { AuthProvider } from "./contexts/AuthContext";
+import { useAuth } from "./contexts/AuthContext";
+import { MetaProvider, useMeta } from "./contexts/MetaContext";
+import { NotesProvider } from "./contexts/NotesContext";
+import ComponentLab from "./pages/ComponentLab";
+import Login from "./pages/Login";
+
+/* Import Components */
+import MainTabs from "./pages/MainTabs";
+import Settings from "./pages/Settings";
+import Signup from "./pages/Signup";
+import TimeLine from "./pages/Timeline";
+import UpdatePassword from "./pages/UpdatePassword";
+import "./theme/global.css";
+
+/* Theme variables */
+import "./theme/variables.css";
 import {
   IonApp,
   IonRouterOutlet,
   setupIonicReact,
   IonSplitPane,
   isPlatform,
-} from '@ionic/react';
-import { useState, useEffect } from 'react'
-import { IonReactRouter } from '@ionic/react-router';
-import { Redirect, Route, useLocation, useHistory } from 'react-router-dom';
-import ReactGA from "react-ga4";
-import { GA_MEASUREMENT_ID } from './config';
-
-/* Import Components */
-import MainTabs from './pages/MainTabs';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import TimeLine from './pages/Timeline';
-import ComponentLab from './pages/ComponentLab'
-import Settings from './pages/Settings';
-import Menu from './components/Menu';
-import Logout from './components/Logout';
-import UpdatePassword from './pages/UpdatePassword';
-import { AuthProvider } from './contexts/AuthContext';
-import { useAuth } from './contexts/AuthContext';
-import { NotesProvider } from './contexts/NotesContext';
-import { MetaProvider, useMeta } from './contexts/MetaContext';
-
+} from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import "@ionic/react/css/core.css";
+import "@ionic/react/css/display.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/float-elements.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
-
-/* Theme variables */
-import './theme/variables.css';
-import './theme/global.css'
-import './App.css'
-
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/typography.css";
+import { useState, useEffect } from "react";
+import ReactGA from "react-ga4";
+import { Redirect, Route, useLocation, useHistory } from "react-router-dom";
 
 /** Ionic Init */
 setupIonicReact();
 
-
 const App: React.FC = () => {
   return (
     <MetaProvider>
-      < AuthProvider >
+      <AuthProvider>
         <NotesProvider>
-          <IonApp className=' app-background'>
+          <IonApp className=" app-background">
             <IonReactRouter>
               <AppContent />
             </IonReactRouter>
           </IonApp>
         </NotesProvider>
-      </AuthProvider >
-    </MetaProvider>)
+      </AuthProvider>
+    </MetaProvider>
+  );
 };
 
 /** Google Analytics Init */
@@ -80,54 +78,58 @@ const AppContent: React.FC = () => {
     setKey(Date.now());
   }, [location.pathname]);
 
-  const isLoginOrSignup = location.pathname === '/login' || location.pathname === '/signup';
-
+  const isLoginOrSignup =
+    location.pathname === "/login" || location.pathname === "/signup";
 
   useEffect(() => {
-    ReactGA.send({ hitType: 'pageview', page: location.pathname + location.search });
+    ReactGA.send({
+      hitType: "pageview",
+      page: location.pathname + location.search,
+    });
     // console.log("GA: ", location.pathname + location.search)
   }, [location]);
 
-
   return (
-    <IonSplitPane key={key} className={`${isLoginOrSignup ? 'max-w-md' : 'max-w-3xl'} mx-auto`} onIonSplitPaneVisible={(event) => setIsSplitPaneOn(event.detail.visible)} contentId="main">
-      {location.pathname !== '/login' && location.pathname !== '/signup' && <Menu />}
+    <IonSplitPane
+      key={key}
+      className={`${isLoginOrSignup ? "max-w-md" : "max-w-3xl"} mx-auto`}
+      onIonSplitPaneVisible={(event) => setIsSplitPaneOn(event.detail.visible)}
+      contentId="main"
+    >
+      {location.pathname !== "/login" && location.pathname !== "/signup" && (
+        <Menu />
+      )}
       <Routes />
     </IonSplitPane>
   );
 };
-
-
-
 
 /**
  * /timeline/pri -> private timeline
  * /timeline/pub -> public timeline
  * /timeline/wor -> workspace timeline
  */
-const Routes: React.FC = ({ }) => {
+const Routes: React.FC = ({}) => {
   const { isAuthenticated, logout } = useAuth();
   const { isSplitPaneOn } = useMeta();
   const history = useHistory();
   useEffect(() => {
     const pathname = history.location.pathname;
-    if (isAuthenticated && (
-      pathname === '/login' ||
-      pathname === '/signup')) {
-      history.push('/timeline/pri');
+    if (isAuthenticated && (pathname === "/login" || pathname === "/signup")) {
+      history.push("/timeline/pri");
     }
 
-    if (!isAuthenticated && (
-      pathname === '/login' ||
-      pathname === '/settings' ||
-      pathname === '/comlab' ||
-      pathname === '/timeline' ||
-      pathname === '/timeline/pri'
-    )) {
-      history.push('/login');
+    if (
+      !isAuthenticated &&
+      (pathname === "/login" ||
+        pathname === "/settings" ||
+        pathname === "/comlab" ||
+        pathname === "/timeline" ||
+        pathname === "/timeline/pri")
+    ) {
+      history.push("/login");
     }
   }, [isAuthenticated, history]);
-
 
   return (
     <IonRouterOutlet id="main" animated={!isSplitPaneOn}>
