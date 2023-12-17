863764d1ea36bed1d9b4d9f636e2b234ef1cba02
Alex T <79433869+hereAlexT@users.noreply.github.com>
2023-12-17 17:13:11 +1100

    added icons

---
+++ frontend/src/components/Icons/Italic.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Italic: React.FC = () => {
+  // Add your component logic here
+
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="16"
+      width="12"
+      viewBox="0 0 384 512"
+    >
+      <path d="M128 64c0-17.7 14.3-32 32-32H352c17.7 0 32 14.3 32 32s-14.3 32-32 32H293.3L160 416h64c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H90.7L224 96H160c-17.7 0-32-14.3-32-32z" />
+    </svg>
+  );
+};
+
+export default Italic;
---
+++ frontend/src/components/Icons/index.tsx
@@ -0,0 +1,1 @@
+export { Bold as default } from "./Bold";
