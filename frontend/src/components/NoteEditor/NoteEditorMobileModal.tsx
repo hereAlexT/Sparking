5df177e4765ff73474ec92d8e5eae65c793c947c
Alex T <79433869+hereAlexT@users.noreply.github.com>
2023-12-17 21:25:00 +1100

    fix

--- frontend/src/components/NoteEditor/NoteEditorMobileModal.tsx
+++ frontend/src/components/NoteEditor/NoteEditorMobileModal.tsx
@@ -104,6 +104,10 @@ const CardEditorMobileModal: React.FC<CardEditorMobileProps> = ({
           "transform",
           `translate3d(0, -${keyboardHeight}px, 0)`,
         );
+        footerRef.current.style.setProperty(
+          "height",
+          "50%", // adjust this value to your liking
+        );
       }
     };
 
